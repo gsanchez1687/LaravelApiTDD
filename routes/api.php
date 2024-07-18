f0d11c7c7d4ce368b67fc99a5f786ec73674266d
Guiillermo Enrique <gsanchez1687@gmail.com>
2024-07-18 13:04:20 -0500

    passport implementado

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PassportAuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', PassportAuthController::class . '@register')->name('register');
+Route::post('login', PassportAuthController::class . '@login')->name('login');
+
+
+//protected routes
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', PassportAuthController::class . '@logout')->name('logout');
 });
